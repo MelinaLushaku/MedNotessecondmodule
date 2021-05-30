602515504ee5b5eaffd9e7f349ba97d9f9dd635f
MelinaLushaku <63053349+MelinaLushaku@users.noreply.github.com>
2021-05-30 17:39:03 +0200

    Initial commit

---
+++ src/main/java/com/example/mednotes2/Mednotes2Application.java
@@ -0,0 +1,13 @@
+package com.example.mednotes2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mednotes2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mednotes2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mednotes2/Mednotes2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mednotes2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mednotes2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
